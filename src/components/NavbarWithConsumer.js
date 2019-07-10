9f488182fe1c9ff6b479a74da99c4600ab8f9a2f
Gery <ardinegery@gmail.com>
2019-07-10 16:15:56 +0700

    Final Context

--- src/components/NavbarWithConsumer.js
+++ src/components/NavbarWithConsumer.js
@@ -1,32 +1,54 @@
-import React, { Component } from 'react';
+import React, { 
+    // Component,
+    useContext 
+} from 'react';
 import { ThemeContext } from '../context/ThemeContext';
 import { AuthContext } from '../context/AuthContext';
 
-export class NavbarWithConsumer extends Component {
-    render() {
-        return (
-            <AuthContext.Consumer>{(authContext)=>(
-                <ThemeContext.Consumer>{(themeContext)=>{
-                    const {isAuthenticated, toggleAuth} = authContext;
-                    const {isLightTheme, light, dark} = themeContext;
-                    const theme = isLightTheme? light:dark;
-                    return(
-                        <nav style={{background:theme.ui, color:theme.syntax}}>
-                            <h1>Context App</h1>
-                            <div onClick={toggleAuth}>
-                                {isAuthenticated?'Logged In':"Logged Out"}
-                            </div>
-                            <ul>
-                                <li>Home</li>
-                                <li>About</li>
-                                <li>Contact</li>
-                            </ul>
-                        </nav>
-                    )
-                }}</ThemeContext.Consumer>
-            )}</AuthContext.Consumer>
-        )
-    }
+// export class NavbarWithConsumer extends Component {
+//     render() {
+//         return (
+//             <AuthContext.Consumer>{(authContext)=>(
+//                 <ThemeContext.Consumer>{(themeContext)=>{
+//                     const {isAuthenticated, toggleAuth} = authContext;
+//                     const {isLightTheme, light, dark} = themeContext;
+//                     const theme = isLightTheme? light:dark;
+//                     return(
+//                         <nav style={{background:theme.ui, color:theme.syntax}}>
+//                             <h1>Context App</h1>
+//                             <div onClick={toggleAuth}>
+//                                 {isAuthenticated?'Logged In':"Logged Out"}
+//                             </div>
+//                             <ul>
+//                                 <li>Home</li>
+//                                 <li>About</li>
+//                                 <li>Contact</li>
+//                             </ul>
+//                         </nav>
+//                     )
+//                 }}</ThemeContext.Consumer>
+//             )}</AuthContext.Consumer>
+//         )
+//     }
+// }
+
+const NavbarWithConsumer = () => {
+    const {isAuthenticated, toggleAuth} = useContext(AuthContext);
+    const {isLightTheme, light, dark} = useContext(ThemeContext);
+    const theme = isLightTheme? light:dark;
+    return(
+        <nav style={{background:theme.ui, color:theme.syntax}}>
+            <h1>Context App</h1>
+            <div onClick={toggleAuth}>
+                {isAuthenticated?'Logged In':"Logged Out"}
+            </div>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
 }
 
 export default NavbarWithConsumer
--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -1,14 +1,24 @@
-import React, { Component } from 'react'
+import React, { 
+    // Component,
+    useContext 
+} from 'react'
 import { ThemeContext } from '../context/ThemeContext';
 
-export class ThemeToggle extends Component {
-    static contextType = ThemeContext;
-    render() {
-        const{toggleTheme} = this.context;
-        return (
-            <button onClick={toggleTheme}>Toggle the Theme</button>
-        )
-    }
+// export class ThemeToggle extends Component {
+//     static contextType = ThemeContext;
+//     render() {
+//         const{toggleTheme} = this.context;
+//         return (
+//             <button onClick={toggleTheme}>Toggle the Theme</button>
+//         )
+//     }
+// }
+
+const ThemeToggle = () => {
+    const {toggleTheme} = useContext(ThemeContext);
+    return (
+        <button onClick={toggleTheme}>Toggle the Theme</button>
+    )
 }
 
 export default ThemeToggle
